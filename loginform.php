dd3627a296231523a31915c9a0647e822703d719
Avesh Momin <97038019+AveshMomin@users.noreply.github.com>
2022-01-31 12:24:57 +0530

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -1,9 +1,27 @@
 <html>
-<?php 
+<?php
+include 'db.php';
+session_start();
+$conn = $con3;
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+        if(isset($_POST["submit"]))
+        {
+                 if(!empty($_POST['username']) && !empty($_POST['password']))
+                  {
+                     $username = ($_POST['username']);
+                     $password = ($_POST['password']);
 
-//connecting to database
-$connect=mysqli_connect("localhost","root","","demo") or die("connection failed");
-
+                     $sql = "INSERT INTO userinfo(username,password) VALUES ('$username','$password')";
+                     if(mysqli_query( $conn, $sql))
+                      {
+                        $_SESSION['username'] = $username;
+                        $username = $_SESSION['username'];
+                        header("location: start.php? username=".$_SESSION['username']);
+                      }
+                     }
+        }
+}
 
 ?>
 <!---form in html--->
@@ -13,15 +31,12 @@ $connect=mysqli_connect("localhost","root","","demo") or die("connection failed"
     <body>
         <div class="loginbox">
             <h1> Login here </h1>
-            <form action='#' method = 'post'>
-             <p>Username</p>
-             <input type="text" name= "uname" placeholder="enter username">
-             <p>Password</p>
-             <input type="password" name= "pwd" placeholder="enter password">
-             <br>
-             <center>
-             <input type="submit" name="save" value="Login">
-            </center> 
+            <form name="form" action="#" method="POST">
+             <p>username</p>
+             <input type="text" name="username" placeholder="Enter Username">
+             <p>password</p>
+             <input type="password" name="password" placeholder="Enter Password">
+             <input type="submit" name="submit" value="Login">
             </form>
 </div>
 </body>
