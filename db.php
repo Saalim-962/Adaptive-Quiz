fc8a45748a3b65353e6036f8e623e1c4b1e3d219
Avesh Momin <97038019+AveshMomin@users.noreply.github.com>
2022-01-31 12:25:38 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,17 @@
-<?php
-//do not change the conatin
+ <?php
+
 $dbhost ="localhost";
 $dbuser = "root";
-$dbpass = "Enter your database password here";
+$dbpass = "avesh@19";
 $dbname1 = "level0";
 $dbname2 = "level1";
+$dbname3 = "userdata";
+$dbname4 = "score";
 
 $con1 = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname1);
 $con2 = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname2);
+$con3 = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname3);
+$con4 = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname4);
 
 if(mysqli_connect_errno())
 {
