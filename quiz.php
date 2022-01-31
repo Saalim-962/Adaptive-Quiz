89b01ec18f825c4757267a39d197d8b40b214975
Avesh Momin <97038019+AveshMomin@users.noreply.github.com>
2022-01-31 12:28:02 +0530

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -5,6 +5,7 @@ session_start();
 $number = $_GET['n'];
 $i = $_GET['i'];
 $counter = $_GET['c'];
+$current_username = $_GET['username'];
 if($i ==1)
 {
     $conn = $con1;
@@ -36,47 +37,41 @@ $total_questions = mysqli_num_rows(mysqli_query($conn,$query));
         <h1>Question Number <?php echo $number; ?> of <?php echo $total_questions; ?></h1>
         <hr>
         <br>
-        </center>
-    
-    
-    <div class="col-lg-8 m-auto d-block">
-    
-    <div class="question">
-        <h2>
-                  
-    <?php 
-        echo $mcq['question'];
-    ?>
-   
-    </h2>
-    </div>
-    <div class="options">
-    <form action="process.php" method="post">
-        <ul>
-        <?php while($row = mysqli_fetch_assoc($options)) { ?>
-            <h3>
-            <li>
-                <input type="radio" name="choice" value="<?php echo $row['ansid'];?>"><?php echo $row['answer'];?>
-            </li>
-            </h3>
-        <?php  }
-        ?>
-        </ul>
-    <input type="hidden" name="i" value="<?php echo $i; ?>">
-    <input type="hidden" name="c" value="<?php echo $counter; ?>">
-    <input type="hidden" name="number" value="<?php echo $number; ?>">
-    </div>
-    <center>
-    <div >
-        <h4>
-    <input type="submit" class="btn" name="submit" value="Submit">
-    </h4>
-    </div>
-    </center>
-    </div>
-</form>
-</div>
-</body>
+        <div class="container">
+            <div class="question">
+                <h2>
+                  <?php
+                    echo $mcq['question'];
+                    ?>
+                  </h2>
+                </div>
+                <div class="options">
+                  <form action="process.php" method="post">
+                    <ul>
+                      <?php while($row = mysqli_fetch_assoc($options)) { ?>
+                        <h3>
+                          <li>
+                            <input type="radio" name="choice" value="<?php echo $row['ansid'];?>"><?php echo $row['answer'];?>
+                          </li>
+                        </h3>
+                      <?php  }
+                      ?>
+                    </ul>
+                    <input type="hidden" name="i" value="<?php echo $i; ?>">
+                    <input type="hidden" name="username" value="<?php echo $current_username; ?>">
+                    <input type="hidden" name="c" value="<?php echo $counter; ?>">
+                    <input type="hidden" name="number" value="<?php echo $number; ?>">
+                  </div>
+                  <center>
+                    <div >
+                      <h4>
+                        <input type="submit" class="btn" name="submit" value="Submit">
+                      </h4>
+                    </div>
+                  </center>
+                </div>
+              </div>
+            </form>
+          </div>
+        </body>
 </html>
-
-
