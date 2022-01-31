cb49635fc3ec271f8045288df73d72d729532495
Avesh Momin <97038019+AveshMomin@users.noreply.github.com>
2022-01-31 12:27:13 +0530

    Update start.php

--- start.php
+++ start.php
@@ -1,17 +1,16 @@
 <?php
-include 'db.php';
-session_start();
-
+  include'db.php';
 ?>
 <?php
+session_start();
 $msg= 'Welcome ' . $_SESSION['username'] . ' Good to see you !';
 
 echo "<script type='text/javascript'>alert('$msg');</script>";
 
 ?>
 <?php
-
 $conn = $con1;
+$username = $_GET['username'];
 $query = "SELECT * FROM questions";
 $total_questions = mysqli_num_rows(mysqli_query($conn,$query));
 
@@ -22,31 +21,30 @@ $total_questions = mysqli_num_rows(mysqli_query($conn,$query));
 	<title>Adaptive Quiz</title>
 	<link rel="stylesheet" type="text/css" href="css/style_start.css">
 </head>
-<body url="https://t4.ftcdn.net/jpg/04/39/13/37/360_F_439133763_FrLdhZsd5aGC23r9ATARuKJBr8ifZjIe.jpg">
+<body >
 
 	<header>
-		<div class="container">
-			<p> Quiz</p>
-		</div>
 	</header>
 
 	<main>
 			<div class="container">
-				<h2>Adaptive Quiz</h2>
+				<center><h1>Adaptive Quiz</h1></center>
+				<hr>
 				<p>
-					This is a multiple choise quiz to test your Knowledge.
+					<center><h3> This is a multiple choise quiz to test your Knowledge.</h3></center>
 				</p>
 				<ul>
-					<li><strong>Number of Questions:</strong><?php echo $total_questions; ?> </li>
-					<li><strong>Type:</strong> Multiple Choise</li>
-					<li><strong> Based on Adaptive Mode</strong></li>
-					<li><strong>Contains questions from both easy and hard level</strong></li>
+					<li><strong>Number of Questions:</strong><?php echo $total_questions; ?> </li><br>
+					<li><strong>Type:</strong> Multiple Choice</li><br>
+					<li><strong> Based on Adaptive Mode</strong></li><br>
+					<li><strong>Contains questions from both easy and hard level</strong></li><br>
 
-					
+
 
 				</ul>
-
-				<a href="quiz.php?n=1&i=1&c=0" class="start">Start Now !</a> <!--Changed the parameters.>
+				<center><button class="start">
+				<a href="quiz.php?n=1&i=1&c=0&username="<?php $username;?> >Click here to Start</a>
+				</button></center>
 
 			</div>
 
