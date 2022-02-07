c5e914ee1fbc4e1c432caaa08d63e5c30b4016ac
Saalim-962 <97034580+Saalim-962@users.noreply.github.com>
2022-02-07 10:52:59 +0530

    Update firstpage.php

--- firstpage.php
+++ firstpage.php
@@ -4,10 +4,10 @@
         <h1> WELCOME TO ADAPTIVE QUIZ</h1>
         <body>
                 <a href="admin.html">
-                    <img src="admin.jfif" class="admin" width="250px" height="250px">
+                    <img src="https://vignette.wikia.nocookie.net/fan-fiction-library/images/1/15/Admin.png/revision/latest?cb=20140917130743" class="admin" width="250px" height="250px">
                 </a>
                 <a href="loginform.php">
-                <img src="user.jfif" class="user" width="250px" height="250px">
+                <img src="https://makingthedifference.in/ImageDir/user.png" class="user" width="250px" height="250px">
                 </a>
                 </div>
                 </div>
